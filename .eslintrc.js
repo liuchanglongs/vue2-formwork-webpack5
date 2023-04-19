a5bc9d6d3363e20e72d693aacbda0f30cada0f1c
刘昌隆 <2251086272@qq.com>
2023-04-19 15:52:45 +0800

    feat: xxx

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@
  * @Version: 2.0
  * @Date: 2023-03-15 11:01:04
  * @LastEditors: lcl
- * @LastEditTime: 2023-04-19 15:02:45
+ * @LastEditTime: 2023-04-19 15:33:34
  * @Description: lcl
  */
 module.exports = {
@@ -23,20 +23,20 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'quotes': 2, // 双引号
-    'semi': 1, // 是否需要分号
-    'no-console': 1, // 0是忽略，1是警告，2是报错
-    'space-before-function-paren': 0,
+    // 'quotes': 2, // 双引号
+    // 'semi': 1, // 是否需要分号
+    // 'no-console': 1, // 0是忽略，1是警告，2是报错
+    // 'space-before-function-paren': 0,
     'vue/no-mutating-props': 0,
     'vue/multi-word-component-names': 0,
     'no-unused-vars': 0,
-    'no-undef': 0,
-    'linebreak-style': [0, 'error', 'windows'],
-    'no-debugger': 0,
-    'no-multiple-template-root': 0,
-    'no-v-for-template-key': 1,
-    'vue/no-reserved-keys': 0,
-    'valid-template-root': 1,
-    'quote-props': 0,
+    // 'no-undef': 0,
+    // 'linebreak-style': [0, 'error', 'windows'],
+    // 'no-debugger': 0,
+    // 'no-multiple-template-root': 0,
+    // 'no-v-for-template-key': 1,
+    // 'vue/no-reserved-keys': 0,
+    // 'valid-template-root': 1,
+    // 'quote-props': 0,
   },
 };
