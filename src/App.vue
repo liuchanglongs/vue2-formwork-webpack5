0f6f7e10c6d02fb5a36a98472e008e98931ccb79
刘昌隆 <2251086272@qq.com>
2023-04-19 11:12:35 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
  * @Version: 2.0
  * @Date: 2023-03-15 11:01:04
  * @LastEditors: lcl
- * @LastEditTime: 2023-04-15 19:35:28
+ * @LastEditTime: 2023-04-19 11:12:12
  * @Description: lcl
 -->
 <template>
   <div id="app">
-    <router-view :key="key"></router-view>
+    <router-view :key="key"> </router-view>
   </div>
 </template>
 
@@ -22,6 +22,7 @@ export default {
       return this.$route.params.id || this.$route.name;
     },
   },
+  mounted() {},
 };
 </script>
 
